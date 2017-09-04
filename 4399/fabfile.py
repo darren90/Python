3074350cd227498530577b6fc9c80e0fe0d7fd6b
Tonny <1005052145@qq.com>
2017-09-04 22:31:47 +0800

    fab update

--- 4399/fabfile.py
+++ 4399/fabfile.py
@@ -1,3 +1,5 @@
+#-*- coding: UTF-8 -*-
+
 from fabric.api import  *
 
 env.hosts = ['115.159.120.152']
@@ -9,6 +11,14 @@ def hello():
     print "hello world"
 
 
+# 拉去github最新代码，并且爬取最新的代码
+def spider():
+    with cd('/home/ubuntu/Python/4399'):
+        run('git pull')
+        run('phython api.py')
+
+
+# 拉去github最新代码，并且重新部署api请求
 def deploy():
     with cd('/home/ubuntu/Python/4399'):
         run('git pull')
